9f3614419f9e10e41470bf3ed7f7fa1a10e0af74
zongxili <zongxiliwork@gmail.com>
2019-10-17 21:38:23 +0000

    from sub-branch: built the About page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  resource :about, only: [:show]
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
