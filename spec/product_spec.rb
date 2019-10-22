d21965ab4e56b5e179b75150c1d42b3f2f3c8a57
zongxili <zongxiliwork@gmail.com>
2019-10-22 00:08:46 +0000

    added Product test

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -12,21 +12,25 @@ RSpec.describe Product, type: :model do
     it "is not valid without a name" do
       subject.name = nil
       expect(subject).to_not be_valid
+      expect(subject.errors[:name]).to_not be_empty
     end
 
     it "is not valid without a price" do
       subject.price_cents = nil
       expect(subject).to_not be_valid
+      expect(subject.errors[:price]).to_not be_empty
     end
   
     it "is not valid without a quantity" do
       subject.quantity = nil
       expect(subject).to_not be_valid
+      expect(subject.errors[:quantity]).to_not be_empty
     end
 
     it "is not valid without a category_id" do
       subject.category_id = nil
       expect(subject).to_not be_valid
+      expect(subject.errors[:category]).to_not be_empty
     end
   end
 end
